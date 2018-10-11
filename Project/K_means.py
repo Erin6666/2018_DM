ce4bb89c8850219448a20e067feb41ec7854892b
Young <erin.chen095l@gmail.com>
2018-10-10 21:13:15 -0400

    project1

---
+++ Project/K_means.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 16:11:53 2018
+
+@author: young
+"""
+
+print "Hello k-means"
---
+++ Project/TestPython/Test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  3 16:11:16 2018
+
+@author: young
+"""
+
+print "Hello Python"
